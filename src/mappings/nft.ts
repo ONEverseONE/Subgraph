87714a88aa1d248062887600921cc41ab971245b
Arpit Bhardwaj <arpitbhardwaj@Arpits-MacBook-Pro-2.local>
2022-06-03 02:22:22 +0530

    Fixed bugs

--- src/mappings/nft.ts
+++ src/mappings/nft.ts
@@ -7,15 +7,9 @@ export function handleTransfer_NFTs(event: Transfer): void {
   let token = NFT.load(id);
   if (token == null) {
     token = new NFT(id);
-  }
-  let newOwner = event.params.to.toHexString();
-  if (
-    newOwner !== "0x1fb8e90fb74dac4ea43b7341cb0f7acc70c93e63" &&
-    newOwner !== "0x1b60fb41d26836604bce7daa13dc65c60aaa9307"
-  ) {
-    token.owner = event.params.to.toHexString();
     token.type = 0;
   }
+  token.owner = event.params.to.toHexString();
   token.contract = event.address.toHexString();
   token.save();
 }
